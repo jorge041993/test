3f5ee8c7b71bc69d01f4c196e6a6a1f899052906
Jorge041993 <jsd_99@hotmail.com>
2017-08-24 09:27:34 -0600

    initial commit adding structor of directories and creating the firt file

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+var altura = 7;
+var base = 5;
+var pi = 3.1416;
+var radio = 2;
+var area = base * altura;
+var areat = (base * altura) /2;
+var areac = pi * (radio * radio);
+var area_esfera = 4/3 * pi * ((radio * radio)* radio);
+console.log("el area de mi rectangulo es igual:" +area);
+console.log("le area del tringulo es:" +areat);
+console.log("el area del circulo es:" +areac);
+console.log("el volumen de la esfera es:" +area_esfera);
+//el area de mi rectangulo es igual:35
+//le area del tringulo es:17.5
+//el area del circulo es:12.5664
+//el volumen de la esfera es:33.5104
