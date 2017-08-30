e5a389298734eceefbdcd08351d3cc04e2611aa8
Jorge041993 <jsd_99@hotmail.com>
2017-08-30 09:51:47 -0600

    palabras

---
+++ www/js/platzom.js
@@ -0,0 +1,22 @@
+//si la palabra termina en "ar", se le quitan los dos ultimos caracteres
+function platzom(str){
+    let translation = str;
+
+    if(str.toLowerCase().endsWith("ar")){
+        translation = str.slice(0, -2);
+    }
+
+//si la palabra inicia con "z", se le añade los caracteres "pe", al final de la palabra  
+if (str.toLowerCase().startsWith("z")){
+    translation +=  "pe"
+}
+
+    return translation;
+}
+console.log(platzom("programar"));
+console.log(platzom("zorro"));
+
+
+//si la palabra traducida tiene 10 o mas letras se deve partir a la mitad se debe unir con un guion
+/*si la palabra original es un palindromo ninguna de las otras reglas funciona si se debuelbe al palabra intercanlando
+ entre mayusculas y minusculas*/
