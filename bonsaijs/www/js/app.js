636bf1d3cc9a6294a14f70e844c5c5c9e976f853
jorge041993 <jsd_@hotmail.com>
2017-09-11 10:16:36 -0600

    bonsai

---
+++ bonsaijs/www/js/app.js
@@ -0,0 +1,13 @@
+new Rect(10, 10, 100, 100)
+.addTo(stage)
+.attr(`fillColor`, `green`)
+.animate(new KeyframeAnimation('2s',{
+    from:{x:0, y:0},
+    '0.5%':{x:300, y:300},
+    to:{x:0, y:0}
+}));
+
+
+/*new Circle(500, 5000, 100)
+.addTo(stage)
+.attr(`fillColor`, `yellow`)*/
