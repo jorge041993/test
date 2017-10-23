c6e6c06ceb7ae50e7dfa59097c9aa2481c75bc9e
jorge041993 <jsd_@hotmail.com>
2017-10-23 09:53:14 -0600

    mejorando trabajo helloworld

--- helloworld/js/scene.js
+++ helloworld/js/scene.js
@@ -1,5 +1,6 @@
 /*clousent*/
 (function(){
+    /*para crear nuestra escena*/
     let scene = new THREE.Scene();
     const aspectRadio = window.innerWidth / window.innerHeight;
     
@@ -10,6 +11,29 @@
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
+
+    /*posicion de la camara*/
+    camera.position.z = 2;
+    camera.position.y = 2;
+
+    let geometry = new THREE.BoxGeometry(1,1,1,1);
+
+/*material de la figura que vamos a crear*/
+    let groundMaterial = new THREE.MeshPhongMaterial({
+        color: 0xfffff
+    });
+
+    /*es el que genera el fractal*/
+    let mesh =new THREE.Mesh(geometry, groundMaterial);
+
+    /*crear luz mercurial*/
+    let pointLight = new THREE.PointLight(0x404040);
+
+    pointLight.position.y = 80;
+
+    scene.add(mesh);
+    scene.add(new THREE.AmbientLight(0x404040));
+    scene.add(pointLight);
     
     renderer.render(scene, camera);
 
